091bb9473c0f3b5acca6a8163271633a70b9d36b
IlyaFinkelshteyn <ilyaf@appveyor.com>
2017-07-15 20:32:12 -0700

    initial checkin

---
+++ VsCodeCoverageTests/Class1Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VsCodeCoverage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsCodeCoverage.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            Class1 cl1 = new Class1();
+            Assert.AreEqual(5, cl1.Sum(3, 2));
+        }
+    }
+}
